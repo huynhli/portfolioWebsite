7051e941388fdd75e2bc547bfaedfa21d4cd749f
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-05-08 01:43:01 -0400

    temp stash from laptop to pc

---
+++ portfolioWebsite/src/Header.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+export default function Header() {
+    return (
+        <>
+            <div className='justify-content-stretch items-stretch'>
+                <h2 className='justify-self-start'>Header1</h2>
+                <h2>Header2</h2>
+                <h2 className='justify-self-end'>Header3</h2>
+            </div>
+        </>
+    )
+}
