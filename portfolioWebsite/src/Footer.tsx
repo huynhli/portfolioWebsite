f59f9d9821ee085dc187c3cc8f9f5939740f7a64
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-05-08 03:17:44 -0400

    working out tailwind once more
    npm create vite@latest
    npm install
    npm install react-router-dom
    npm install tailwindcss @tailwind.css/vite

--- portfolioWebsite/src/Footer.tsx
+++ portfolioWebsite/src/Footer.tsx
@@ -1,14 +1,12 @@
 
-export default function Header() {
+export default function Footer() {
     return (
         <div className='flex justify-center items-center bg-blue-800 h-20 
         border-b border-blue-900'>
-            <div className="flex space-x-8 py-4 bg-blue-800 h-20 
-        border-b border-blue-900">
+            <div className="flex space-x-8 py-4">
                 <button className="text-lg px-6 py-3 text-white 
                 transition-all duration-300 hover:text-2xl hover:bg-blue-700 
-                rounded-lg hover:cursor-pointer bg-blue-800 h-20 
-        border-b border-blue-900">
+                rounded-lg hover:cursor-pointer">
                     Home
                 </button>
                 <button className="text-lg px-6 py-3 text-white 
---
+++ portfolioWebsite/src/HomePage.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return (
+
+        <>
+            <h2 className="text-3xl font-bold underline bg-blue-800 h-20 
+        border-b border-blue-900">Body</h2>
+        </>
+
+    )
+}
