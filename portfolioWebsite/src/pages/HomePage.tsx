44bead0390f7f7937295352012061129f709f9a7
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-08-21 20:40:38 -0400

    card animation progress
    temp stash to go home

--- portfolioWebsite/src/pages/HomePage.tsx
+++ portfolioWebsite/src/pages/HomePage.tsx
@@ -1,14 +1,15 @@
 // import { useState, useEffect } from "react"
-import { motion, useScroll, useSpring, useTransform } from "framer-motion"
+import { motion, useScroll, useTransform } from "framer-motion"
 
 export default function HomePage() {
     const { scrollYProgress } = useScroll()
 
     const wavesY = useTransform(scrollYProgress, [0, 0.4], [100, 0])
     const wavesScaleY = useTransform(scrollYProgress, [0, 0.2], [2, 1])
-    const bottomWavesY = useTransform(scrollYProgress, [0.5, 0.6], [0, 700])
-    const bottomWavesSpring = useSpring(bottomWavesY, { stiffness: 100, damping: 10})
+    const bottomWavesY = useTransform(scrollYProgress, [0.4, 0.6], [-100, 700])
+    // const bottomWavesSpring = useSpring(bottomWavesY, { stiffness: 100, damping: 10})
     // stiffness = spring force, damping = resistance 
+
     // const [allArticles, setAllArticles] = useState<{
     //     id: string
     //     title: string
@@ -67,9 +68,11 @@ export default function HomePage() {
         <div className='relative flex flex-col'>
 
             <section className="relative flex flex-col h-200 justify-center items-center">
-                <motion.div className="absolute pointer-events-none z-[1] w-[100%] h-[100%] z-10 "
+                <motion.div className="absolute pointer-events-none z-[-1] w-[100%] h-[100%] z-10 "
                     style={{ opacity: 0}}
                     animate={{ opacity: 1 }}
+                    // x, y, scale(x/y), rotate, rotateX, rotateY, skewX, skewY, opacity, colours, width, height, padding,margin, border radius, filter, zindex
+
                     transition={{ duration: 2, delay: 0.2, ease: "easeInOut" }}
                     // duration, delay, ease, repeat, repeatType, spring. per property transition
                 >
@@ -78,21 +81,41 @@ export default function HomePage() {
                     ))}
                 </motion.div>
 
-                <motion.div
-                    style={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 4, delay: 0.2, ease: "easeInOut" }}
-                    className="flex flex-col z-50 w-[25%] min-w-90 max-w-100 h-50 px-5 bg-zinc-900 border-1 border-white rounded-lg justify-center"
-                >
-                    <h1 className="text-white text-[clamp(2rem,2vw,2rem)]">Frontend Developer</h1>
-                    <h2 className="text-white text-[clamp(1rem,1vw,1rem)] mb-5">Hi! I'm Liam, a Frontend Developer dedicated to making scalable, high-performance user-centered web solutions.</h2>
-                </motion.div>
+                <div className="
+                    bg-white h-100 pb-10 px-[20%] pt-30 w-[80%]
+                    group relative flex flex-row justify-center items-center
+                    ">
+                    <motion.div
+                        style={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 4, delay: 0.2, ease: "easeInOut" }}
+                        className="
+                            flex flex-col z-50 pointer-events-none w-[25%] min-w-90 max-w-100 h-50 px-5 bg-zinc-900 border-1 border-white rounded-lg justify-center
+                            transition duration-400 delay-80 group-hover:scale-115 group-hover:-rotate-3 group-hover:-translate-x-50 group-hover:-translate-y-10
+                            "
+                    >
+                        <h1 className="text-white text-[clamp(2rem,2vw,2rem)]">Frontend Developer</h1>
+                        <h2 className="text-white text-[clamp(1rem,1vw,1rem)] mb-5">Hi! I'm Liam, a Frontend Developer dedicated to making scalable, high-performance user-centered web solutions.</h2>
+                    </motion.div>
+                    <div
+                        className="
+                            absolute flex-col z-40 w-[25%] min-w-90 max-w-100 h-50 px-5 bg-zinc-900 border border-white rounded-lg justify-center
+                            opacity-0 translate-x-0
+                            group-hover:opacity-100 group-hover:translate-x-60
+                            transition-all duration-400 ease-in-out
+                        "
+                    >
+                        <h1 className="text-white text-[clamp(2rem,2vw,2rem)]">Second section</h1>
+                        <h2 className="text-white text-[clamp(1rem,1vw,1rem)] mb-5">Here is more info about me.</h2>
+                    </div>
+                </div>
 
-                
+                {/* resume button that follows you */}
+
                 
             </section>
             
-            <motion.section className="absolute w-full top-100 z-10 scale-y-50"
+            <motion.section className="pointer-events-none absolute w-full top-100 z-10 scale-y-50"
                 // style = start values, animate = end values, transition for type 
                 style={{ y: wavesY, scaleY: wavesScaleY }}
                 
@@ -112,24 +135,17 @@ export default function HomePage() {
             </section>
             
 
-            <motion.section className="relative flex-col top-[-400px] w-full z-10 scale-y-50"
+            <motion.section className="pointer-events-none relative flex-col top-[-400px] w-full z-10 scale-y-50"
                 // style = start values, animate = end values, transition for type 
-                style={{ y: bottomWavesSpring }}
+                style={{ y: bottomWavesY }}
                 
             >
                 <img src='/images/waves2.png' className="w-full h-[400px] object-fill" />
                 <img src='/images/waves.png' className="w-full h-[400px] object-fill rotate-180" />
             </motion.section>
-            {/* description section
-            <div className="flex justify-center">
-                    <h2 className="self-center my-30 text-3xl w-170 mx-5 bg-purple-300 rounded-lg p-5">
-                        Hi, I'm Liam - a game developer and designer as well as a full-stack web developer passionate about creating engaging experiences and bringing ideas to life.
-                    </h2>
-            </div>
 
-
+            {/* 
             <div className="flex justify-center p-4">
-
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-6xl">
                     {isLoading ? ( 
                         <div className="col-span-full text-center py-8 mb-5 text-3xl font-bold"><p>{loadingText} If nothing loads for a while, please load <a href='https://liamportfolioweb.onrender.com/' className='text-blue-400 hover:text-blue-500 underline'>this page</a> and come back.</p></div>
